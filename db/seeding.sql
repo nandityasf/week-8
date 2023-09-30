1849adecb309782f8ee1cdbd082f4fa8d8cd8095
nandityasf <you@example.com>
2023-09-30 19:20:47 +0700

    seeding.sql file is here

--- db/seeding.sql
+++ db/seeding.sql
@@ -1,2 +1,2 @@
 INSERT INTO actor (actor_id, first_name, last_name, last_update)
-VALUES (201, 'Dave', 'Franco', 'now'), (202, 'Idris', 'Elba', 'now'), (203, 'Benedict', 'Cumberbatch', 'now'), (204, 'Albus', 'Dumbledore', 'now'), (205, 'Draco', 'Malfoy', 'now')
+VALUES (201, 'Dave', 'Franco', 'now'), (202, 'Idris', 'Elba', 'now'), (203, 'Benedict', 'Cumberbatch', 'now'), (204, 'Albus', 'Dumbledore', 'now'), (205, 'Draco', 'Malfoy', 'now') 
